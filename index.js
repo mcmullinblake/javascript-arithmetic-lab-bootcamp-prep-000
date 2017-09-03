b1babe113c7cd73dfcf041ef262b442d91039c4d
blake mcmullin <mcmullin.blake@gmail.com>
2017-09-03 23:32:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-function(add) {
-  return.(a + b)
+function add(a, b) {
+  return a + b 
 }
 
-function(subtract) {
-  return.(a - b)
+function subtract(a, b) {
+  return a - b 
 }
 
-function(multiply) {
-  return.(a * b)
+function multiply(a, b) {
+  return a * b 
 }
 
 function(divide) {
